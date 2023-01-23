cd456495a60ebc7d736920313e3889e26e10e0fc
Justin Stendara <jnstendara@gmail.com>
2023-01-23 12:19:15 -0600

    Updated some files with the commands I am using to do ETL. Currently have users and questions figured out. Need to figure out answers and users next.

---
+++ etl/questionDataMerge.sql
@@ -0,0 +1,30 @@
+CREATE TABLE if not exists temp_questions (
+  question_id SERIAL NOT NULL,
+  product_id bigint,
+  body varchar(1000),
+  date_written bigint,
+  asker_name varchar(60),
+  asker_email varchar(60),
+  reported bit(1),
+  helpful INTEGER,
+);
+
+\copy  temp_questions
+FROM '/Users/justinstendara/Documents/HackReactor/Git/seniorPhase/sdc/qna/input_data/questions.csv'
+csv header;
+
+ALTER TABLE temp_questions INSERT user_id INTEGER;
+
+UPDATE temp_questions
+SET
+user_id = u.user_id
+FROM users u
+WHERE temp_questions.asker_name = u.name and temp_questions.asker_email = u.email;
+
+ALTER TABLE temp_questions DROP COLUMN asker_name;
+ALTER TABLE temp_questions DROP COLUMN asker_email;
+ALTER TABLE temp_questions RENAME COLUMN date_written TO date;
+
+INSERT INTO questions
+SELECT question_id, product_id, body, reported, date,  helpful, user_id
+FROM temp_questions;
---
+++ etl/userData.sql
@@ -0,0 +1,14 @@
+-- Before running this command, run the userData.js file and ensure that the
+-- 'myTest.csv' file was generated without any issues
+
+CREATE TEMP TABLE temp_users_table
+AS
+SELECT *
+FROM users
+WITH NO DATA;
+
+\copy  temp_users_table from '/Users/justinstendara/Documents/HackReactor/Git/seniorPhase/sdc/qna/input_data/myTest.csv' csv header;
+
+INSERT INTO users (name, email)
+SELECT DISTINCT name, email
+FROM temp_users_table;
