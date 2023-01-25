789d3c03ee05841faa00084d0320f7676e786510
Justin Stendara <jnstendara@gmail.com>
2023-01-25 17:51:06 -0600

    Added helpfulQuestion which needs to be tested and added empty report functions for answers and questions

--- controllers/putRequests.js
+++ controllers/putRequests.js
@@ -3,8 +3,26 @@ let helpfulAnswer = (db, answer_id) => {
   return db.query(query, [answer_id])
     .then(results => (results))
     .catch(err => (err));
-}
+};
+
+let helpfulQuestion = (db, question_id) => {
+  let query = 'UPDATE questions SET helpfulness = helpfulness + 1 WHERE question_id = $1';
+  return db.query(query, [question_id])
+    .then(results => (results))
+    .catch(err => (err));
+};
+
+let reportAnswer = (db, answer_id) => {
+
+};
+
+let reportQuestion = (db, question_id) => {
+
+};
 
 module.exports = {
   helpfulAnswer: helpfulAnswer,
+  helpfulQuestion: helpfulQuestion,
+  reportAnswer: reportAnswer,
+  reportQuestion: reportQuestion
 }
