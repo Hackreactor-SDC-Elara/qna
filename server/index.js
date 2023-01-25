9ed2d80ffe11a87e1b4cc2279708cffb346ebb2e
Justin Stendara <jnstendara@gmail.com>
2023-01-24 21:04:42 -0600

    Added routes to server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,64 @@ app.get('/', (req, res) => {
   res.send('hi');
 })
 
+// GET '/qa/questions' => requires product_id, page, count
+//   Should return 200 if it was completed
+app.get('/qa/questions', (req, res) => {
+  req.query.page = req.query.page ?? 1;
+  console.log('User has requested question information with the following parameters: ', req.query);
+  res.send(req.query);
+});
+
+// GET '/qa/questions/:question_id/answers' => requires question_id, page, and count
+//   Shoudl return 200 if it was completed
+app.get('/qa/questions', (req, res) => {
+  req.query.page = req.query.page ?? 1;
+  console.log('User has requested answer information with the following parameters: ', req.query);
+  res.send(req.query);
+});
+
+// POST '/qa/questions' => requires body, name, email, and product_id
+//   Should return 201 if successful
+app.post('/qa/questions', (req, res) => {
+  console.log('User has tried to post a new question: ', req.query);
+  res.send(req.query);
+});
+
+// POST '/qa/questions/:question_id/answers' => requires body, name, email, and photos(opt)
+//   Should return 201 if successful
+app.post('/qa/questions', (req, res) => {
+  console.log('User has tried to answer a question: ', req.query);
+  res.send(req.query);
+});
+
+// PUT requires question_id
+//   Should return 204 if successful
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  console.log('User has tried to mark a question helpful: ', req.query);
+  res.send(req.query);
+});
+
+// PUT requires question_id
+//   Should return 204 if successful
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  console.log('User has tried to report a question: ', req.query);
+  res.send(req.query);
+});
+
+// PUT requires question_id
+//   Should return 204 if successful
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  console.log('User has tried to mark an answer helpful: ', req.query);
+  res.send(req.query);
+});
+
+// PUT requires question_id
+//   Should return 204 if successful
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  console.log('User has tried to report an answer: ', req.query);
+  res.send(req.query);
+});
+
 app.listen(PORT, () => {
   console.log(`Server up, listening to port ${PORT}`);
 });
