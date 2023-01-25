6c36cd62f66d7fc05625b29a1956af423b7d591f
Justin Stendara <jnstendara@gmail.com>
2023-01-25 17:47:43 -0600

    Clenared up putRequests file

--- controllers/putRequests.js
+++ controllers/putRequests.js
@@ -1,16 +1,8 @@
 let helpfulAnswer = (db, answer_id) => {
   let query = 'UPDATE answers SET helpfulness = helpfulness + 1 WHERE answer_id = $1';
-
-  console.log('Here we are!', answer_id)
   return db.query(query, [answer_id])
-    .then(results => {
-      console.log('resuts: ', results)
-      return results;
-    })
-    .catch(err => {
-      console.log('rerr: ', err)
-      return err;
-    });
+    .then(results => (results))
+    .catch(err => (err));
 }
 
 module.exports = {
