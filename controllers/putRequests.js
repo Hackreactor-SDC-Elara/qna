33ac3ce9ba1b3c86b568e6f1e98ab8c7102b1d9f
Justin Stendara <jnstendara@gmail.com>
2023-01-26 11:04:16 -0600

    Added functionality and tests for report question and answers

--- controllers/putRequests.js
+++ controllers/putRequests.js
@@ -13,11 +13,19 @@ let helpfulQuestion = (db, question_id) => {
 };
 
 let reportAnswer = (db, answer_id) => {
+  let query = 'UPDATE answers SET reported = B\'1\' WHERE answer_id = $1';
 
+  return db.query(query, [answer_id])
+    .then(results => (results))
+    .catch(err => (err));
 };
 
 let reportQuestion = (db, question_id) => {
+  let query = 'UPDATE questions SET reported = B\'1\' WHERE question_id = $1';
 
+  return db.query(query, [question_id])
+    .then(results => (results))
+    .catch(err => (err));
 };
 
 module.exports = {
