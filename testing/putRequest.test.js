5eddeaf07f78f0ac02638d15cd4a8ac3020b1c37
Justin Stendara <jnstendara@gmail.com>
2023-01-26 10:44:45 -0600

    Added tests for helpful question put request

--- testing/putRequest.test.js
+++ testing/putRequest.test.js
@@ -1,4 +1,4 @@
-let {helpfulAnswer} = require('../controllers/putRequests.js');
+let {helpfulAnswer, helpfulQuestion} = require('../controllers/putRequests.js');
 let {client} = require('./connectToDBTest.js');
 
 describe('Answer helpfulness put requests should be functional', () => {
@@ -18,6 +18,27 @@ describe('Answer helpfulness put requests should be functional', () => {
   // Sad path
   it('Should do nothing if no answer_id is supplied', async () => {
     let result = await helpfulAnswer(client);
+    expect(result.rowCount).toBe(0);
+  });
+});
+
+describe('Question helpfulness put requests should be functional', () => {
+  // Happy path
+  it('Should Increment the helpfulness of an question.', async () => {
+    let initialQuestion = await client.query('SELECT * FROM questions WHERE question_id = 139874');
+    await helpfulQuestion(client, '139874');
+    let finalQuestion = await client.query('SELECT * FROM questions WHERE question_id = 139874');
+
+    expect(parseInt(initialQuestion.rows[0].helpfulness))
+      .toBeLessThan(parseInt(finalQuestion.rows[0].helpfulness));
+
+    expect(initialQuestion.rows[0].body)
+      .toEqual(finalQuestion.rows[0].body);
+  });
+
+  // Sad path
+  it('Should do nothing if no question_id is supplied', async () => {
+    let result = await helpfulQuestion(client);
     expect(result.rowCount).toBe(0);
   });
 });
